926acce3e09cb3465cd9fd540e3e5fb155a43762
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-05-29 05:30:19 +0300

    feat: add script that prints 'C is fun' based on number of occurences

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,10 @@
 const x = parseInt(process.argv[2]);
-
+let message = 'C is fun'
 if(isNaN(x)){
 console.log('Missing number of occurrences');
 }
 else{
 for(let i = 0; i < x; i++){
-console.log('C is fun');
+console.log(message);
 }
 }
